41aa9e4b0e5c8e35849ed65eae29924172f8f7d6
xusheng <xusheng666@gmail.com>
2018-04-21 12:49:03 +0800

    the initial of VMCS for SPL CA

---
+++ VMCS Sources/setenv.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_161
+set ANT_HOME=D:\apache-ant-1.9.10
+set JUNIT_HOME=.\jars
+
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set CLASSPATH=%JUNIT_HOME%\junit.jar
